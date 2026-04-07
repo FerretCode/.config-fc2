e1135ca84d76fa3f689abffaa95b6b7328cb91aa
FerretCode <eggertet@oregonstate.edu>
2026-04-07 04:36:59 -0700

    add niri configuration

--- wpg/wp_init.sh
+++ wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -nrs '/home/fc/Downloads/wallpapers/0010.jpg' '/home/fc/Downloads/wallpapers/0010.jpg'
+wpg -nrs '/home/fc/Downloads/wallpapers/0202.jpg' '/home/fc/Downloads/wallpapers/0202.jpg'
